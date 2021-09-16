f90b466bfb2409d69636812302f55e5c6657165b
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-09-16 10:53:20 +0800

    App: added Library container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Container from './components/Container'
+import Library from './components/player/Library'
 import Player from './components/player/Player'
 
 function App() {
   return (
     <Container>
       <Player />
+      <Library />
     </Container>
   )
 }
